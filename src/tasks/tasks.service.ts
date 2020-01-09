27c841e4adee9369861a8e4eaabdff9a619ba803
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-09 10:12:46 +0200

    Refactors GET `/tasks`

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,20 +14,9 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
   ) {}
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(task => task.title.includes(search) || task.description.includes(search));
-  //   }
-  //   return tasks;
-  // }
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return await this.taskRepository.getTasks(filterDto);
+  }
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
     if (!found) throw new NotFoundException(`Task with ID "${id}" does not exist`);
