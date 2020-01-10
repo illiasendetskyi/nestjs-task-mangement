61bdb3269d84fd729e754164289ee314fc069691
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-10 09:44:53 +0200

    Validates username and password

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+//docker run --rm   --name pg-docker -e POSTGRES_PASSWORD=test1234 -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data  postgres
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
