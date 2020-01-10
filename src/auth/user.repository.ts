df25cf4999723a2f1178dc2c0de04c41e732e4da
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-10 09:59:25 +0200

    Makes `username` unique

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,6 +11,17 @@ export class UserRepository extends Repository<User> {
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      console.log(error);
+      //TODO: move to the enums
+      //dup of user name
+      if (error.code === '23505') {
+        throw new ConflictException(`Username "${username}" already exists`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
