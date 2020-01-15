d0a7dbee18629130b4522fe8a9f543dd0c6d68f6
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-15 10:28:04 +0200

    Applies logger

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,4 +1,6 @@
 import { Repository, EntityRepository } from 'typeorm';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
+
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
@@ -7,6 +9,7 @@ import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private loggerr = new Logger('TaskRepository');
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -14,8 +17,16 @@ export class TaskRepository extends Repository<Task> {
     query.where('task.userId = :userId', { userId: user.id });
     if (status) query.andWhere('task.status = :status', { status });
     if (search) query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.loggerr.error(
+        `Failed to get tasks for user "${user.username}", Filters: ${JSON.stringify(filterDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -25,7 +36,15 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
-    await task.save();
+    try {
+      await task.save();
+    } catch (error) {
+      this.loggerr.error(
+        `Failed to create new task for user "${user.username}", Data: ${JSON.stringify(createTaskDto)}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
     delete task.user;
     return task;
   }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,24 +11,28 @@ import {
   ValidationPipe,
   ParseIntPipe,
   UseGuards,
+  Logger,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+import { TaskStatus } from './task-status.enum';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
-import { TaskStatus } from './task-status.enum';
-import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/auth/user.entity';
 import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('/tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto, @GetUser() user: User): Promise<Task[]> {
+    this.logger.verbose(`User "${user.username} retrieving all tasks. Filters ${JSON.stringify(filterDto)}"`);
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -40,6 +44,7 @@ export class TasksController {
   @Post()
   @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto, @GetUser() user: User): Promise<Task> {
+    this.logger.verbose(`User "${user.username} is creating new task. Data ${JSON.stringify(createTaskDto)}"`);
     return this.tasksService.createTask(createTaskDto, user);
   }
 
