aa87a66c0adbfa707c0fe27d6f108a7be41c2081
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-15 08:38:18 +0200

    Returns tasks belonged to the authed user on GET `/tasks`

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,14 +7,14 @@ import { User } from 'src/auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
 
+    query.where('task.userId = :userId', { userId: user.id });
     if (status) query.andWhere('task.status = :status', { status });
     if (search) query.andWhere('(task.title LIKE :search OR task.description LIKE :search)', { search: `%${search}%` });
     const tasks = await query.getMany();
-    console.log(tasks);
     return tasks;
   }
 
