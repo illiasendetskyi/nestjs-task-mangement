1bb1123a76b0306059d7f03bdbd6ac15219813ee
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-20 10:16:44 +0200

    Enables CORS in prod env only from the specified origin

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap(): Promise<void> {
-  const serverConfig: { port: number } = config.get('server');
+  const serverConfig: { port: number; origin: string } = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development') app.enableCors();
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
