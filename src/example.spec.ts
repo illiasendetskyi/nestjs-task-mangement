e46d0185cb717d5fe11c4378fafa1c837253a26a
illiasendetskyi <illia.sendetskyi.dev@gmail.com>
2020-01-22 10:39:33 +0200

    Adds example spec file for testing

---
+++ src/example.spec.ts
@@ -0,0 +1,56 @@
+class FriendsList {
+  friends = [];
+  addFriend(name) {
+    this.friends.push(name);
+    this.announceFriendship(name);
+  }
+
+  announceFriendship(name) {
+    console.log(`${name} is now a friend`);
+  }
+
+  removeFriend(name) {
+    const idx = this.friends.indexOf(name);
+    if (idx === -1) throw new Error('Friend not found');
+    this.friends.splice(idx, 1);
+  }
+}
+describe('FriendsList', () => {
+  let friendsList = null;
+  beforeEach(() => {
+    friendsList = new FriendsList();
+  });
+
+  it('initializes friends list', () => {
+    expect(friendsList.friends.length).toEqual(0);
+  });
+
+  it('adds a friend to the list', () => {
+    friendsList.addFriend('Illia');
+
+    expect(friendsList.friends.length).toEqual(1);
+  });
+
+  it('announces friendship', () => {
+    friendsList.announceFriendship = jest.fn();
+
+    expect(friendsList.announceFriendship).not.toHaveBeenCalled();
+    friendsList.addFriend('Illia');
+    expect(friendsList.announceFriendship).toHaveBeenCalledWith('Illia');
+  });
+
+  describe('removeFriend', () => {
+    it('removes a friend from the list', () => {
+      friendsList.addFriend('Illia');
+      expect(friendsList.friends[0]).toEqual('Illia');
+      friendsList.removeFriend('Illia');
+      expect(friendsList.friends[0]).toBeUndefined();
+    });
+
+    it('throws an error as friend does not exist', () => {
+      expect(() => friendsList.removeFriend('Illia')).toThrow(new Error('Friend not found'));
+
+      // expect(friendsList.friends.length).toEqual(1);
+    });
+  });
+});
